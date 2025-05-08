ccaa64b8bca6c8e3b25665d414d540bedaf06dc9
coffeeaddictamoeba <vika.pokalo.2023@gmail.com>
2025-05-08 22:03:51 +0300

    SCRUM-37 conflicts resolved

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('', views.register, name='register'),
     path('login/', views.user_login, name='user_login'),
+    path('logout/', LogoutView.as_view(next_page='register'), name='logout'),
 ]
