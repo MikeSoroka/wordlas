fc0554208f728a66fb1e85419c378e3a2e77c589
coffeeaddictamoeba <vika.pokalo.2023@gmail.com>
2025-05-08 16:32:28 +0300

    SCRUM-35 added basic user registration

--- project/main/urls.py
+++ project/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('index/', views.index, name='index'),
+    path('', views.register, name='register'),
+    path('login/', views.user_login, name='user_login'),
 ]
